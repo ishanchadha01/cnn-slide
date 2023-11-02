a246f1518e78f2ac6cbc402d0d903146a11e11c7
Manoj Niverthi <manojniverthi@gmail.com>
2023-11-01 23:01:40 -0400

    finished draft of v1

--- conv/sketch_conv.py
+++ conv/sketch_conv.py
@@ -29,8 +29,6 @@ class SketchConv2d(nn.Module):
         self.sketch_dim = sketch_dimension
         self.type1_sketches = [None] * self.num_sketches
         self.type1_signed_matrices = [None] * self.num_sketches
-        self.type2_sketches = [None] * self.num_sketches
-        self.type2_signed_matrices = [None] * self.num_sketches
 
         if sketch_output and sketch_dimension >= out_channels:
             raise Exception(
@@ -52,27 +50,52 @@ class SketchConv2d(nn.Module):
             torch.empty(out_channels).uniform_(-1 * k, k) if self.bias else None
         )
         for idx in range(self.num_sketches):
-            u1 = (
+            # TODO: does reordering args give better performance?
+            self.type1_signed_matrices[idx] = (
                 (
                     (
-                        2
-                        * torch.bernoulli(
-                            torch.empty(in_channels // self.groups, self.sketch_dim)
+                        (
+                            2
+                            * torch.bernoulli(
+                                torch.empty(self.out_channels, self.sketch_dim)
+                            )
                         )
+                        - 1
                     )
-                    - 1
+                    / self.sketch_dim
                 )
-                / self.sketch_dim
-            ).detach()
-            self.type1_signed_matrices[idx] = u1
-            u2 = (
-                ((2 * torch.bernoulli(torch.empty(out_channels, self.sketch_dim))) - 1)
-                / self.sketch_dim
-            ).detach()
-            self.type2_signed_matrices[idx] = u2
-            self.type1_sketches[idx] = torch.einsum(
-                "ijkl, jm -> imkl", self.weight_tensor, u1
-            )  # TODO: does reordering args give better performance?
-            self.type2_sketches[idx] = torch.einsum(
-                "ijkl, im -> mjkl", self.weight_tensor, u2
+                .detach()
+                .T
+            )  # sketch_dim x out_channels
+            # self.type2_signed_matrices[idx] = u2
+            self.type1_sketches[idx] = (
+                torch.einsum(
+                    "ijkl, im -> mjkl",
+                    self.weight_tensor,
+                    self.type1_signed_matrices[idx],
+                )
+                .permute(0, 2, 3, 1)
+                .flatten(0, 2)
+            )  # (in_channels x h x w) x sketch_dim
+
+    def forward(self, x):
+        B, _, H1, W1 = x.shape()
+        H2 = H1 - self.kernel_height + 1
+        W2 = W1 - self.kernel_width + 1
+        input_unfolded = torch.nn.functional.unfold(
+            x, (self.kernel_height, self.kernel_width)
+        )  # batch x (in_channels * h * w) x L
+        result = None
+        for idx in range(self.num_sketches):
+            u1 = self.type1_signed_matrices[idx]
+            s1 = self.type1_sketches[idx]
+            input_unfolded = torch.einsum("ijk, jl -> ilk", input_unfolded, s1)
+            input_unfolded = torch.einsum("ijk, jl -> ilk", input_unfolded, u1).view(
+                B, self.out_channels, H2, W2
             )
+            if not result:
+                result = input_unfolded / self.num_sketches
+            else:
+                result += input_unfolded / self.num_sketches
+        result += self.bias_tensor[None, :, None, None]
+        return result
