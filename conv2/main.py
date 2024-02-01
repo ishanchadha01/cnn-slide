8af4f8b9e0be0b50c2c929ed6ac6f046bedeb0ee
ishan <ishanchadha01@gmail.com>
2024-02-01 11:54:47 -0500

    modified training/testing script for alsh to incorporate current model weights

--- conv2/main.py
+++ conv2/main.py
@@ -1,11 +1,14 @@
 from sketch_conv import SketchConv2d
 from models import MNIST, AlshCNN
 import torch
-from torchvision import transforms, datasets
+from torchvision import transforms, datasets, models
 import torch.nn.functional as F
+import torch.nn as nn
 import torch.optim as optim
 import argparse
 from tqdm import tqdm
+import itertools
+from alsh_conv import AlshConv2d, SRPTable
 
 
 if __name__=='__main__':
@@ -17,14 +20,44 @@ if __name__=='__main__':
     args = parser.parse_args()
 
     device = torch.device("cpu")
-    # model = MNIST().to(device)
-    model = AlshCNN().to(device)
+    model =  models.alexnet(pretrained=True)  # Assuming CIFAR-10 has 10 classes
+    print(model.features)
+    # model = AlshCNN().to(device)
+
+    # construct alsh cnn using current weights
+    # conv1 = AlshConv2d(in_channels=3, out_channels=64, kernel_size=11, stride=4, padding=2, dilation=1, bias=True, is_first_layer=True, is_last_layer=True, num_hashes=5, num_tables=8, max_bits=16, hash_table=SRPTable)
+    # ftr_weight = model.features[0].weight
+    # conv1.weight = ftr_weight
+    # model.features[0] = conv1
+
+    # conv2 = AlshConv2d(in_channels=64, out_channels=192, kernel_size=5, stride=1, padding=2, dilation=1, bias=True, is_first_layer=True, is_last_layer=True, num_hashes=5, num_tables=8, max_bits=16, hash_table=SRPTable)
+    # ftr_weight = model.features[3].weight
+    # conv2.weight = ftr_weight
+    # model.features[3] = conv2
+
+    # conv3 = AlshConv2d(in_channels=192, out_channels=384, kernel_size=3, stride=1, padding=1, dilation=1, bias=True, is_first_layer=True, is_last_layer=True, num_hashes=5, num_tables=8, max_bits=16, hash_table=SRPTable)
+    # ftr_weight = model.features[6].weight
+    # conv3.weight = ftr_weight
+    # model.features[6] = conv3
+
+    # conv4 = AlshConv2d(in_channels=384, out_channels=256, kernel_size=3, stride=1, padding=1, dilation=1, bias=True, is_first_layer=True, is_last_layer=True, num_hashes=5, num_tables=8, max_bits=16, hash_table=SRPTable)
+    # ftr_weight = model.features[8].weight
+    # conv4.weight = ftr_weight
+    # model.features[8] = conv4
+
+    conv5 = AlshConv2d(in_channels=256, out_channels=256, kernel_size=3, stride=1, padding=1, dilation=1, bias=True, is_first_layer=True, is_last_layer=True, num_hashes=5, num_tables=8, max_bits=16, hash_table=SRPTable)
+    ftr_weight = model.features[10].weight
+    conv5.weight = ftr_weight
+    model.features[10] = conv5
+
+
+
     transform = transforms.Compose([
+        transforms.Resize((224, 224)),
         transforms.ToTensor(),
-        transforms.Normalize((0.1307,), (0.3081,))
     ])
-    train_data = datasets.MNIST("../data", train=True, transform=transform, download=False)
-    test_data = datasets.MNIST("../data", train=False, transform=transform, download=False)
+    train_data = datasets.CIFAR10("../data", train=True, transform=transform, download=False)
+    test_data = datasets.CIFAR10("../data", train=False, transform=transform, download=False)
     train_loader = torch.utils.data.DataLoader(train_data, batch_size=args.batch_size)
     test_loader = torch.utils.data.DataLoader(test_data, batch_size=args.batch_size)
 
@@ -35,7 +68,7 @@ if __name__=='__main__':
     for epoch in tqdm(range(1, args.epochs+1)):
         # train
         model.train()
-        for batch_idx, (data, target) in tqdm(enumerate(train_loader)):
+        for batch_idx, (data, target) in tqdm( enumerate(itertools.islice(train_loader, 3)) ):
             data = data.to(device)
             target = target.to(device)
             optimizer.zero_grad()
@@ -53,7 +86,7 @@ if __name__=='__main__':
         pred = None
         correct = 0
         with torch.no_grad():
-            for data, target in tqdm(test_loader):
+            for data, target in tqdm( itertools.islice(test_loader, 3) ):
                 data = data.to(device)
                 target = target.to(device)
                 out = model(data)
--- conv2/models.py
+++ conv2/models.py
@@ -33,32 +33,38 @@ class MNIST(nn.Module):
 class AlshCNN(nn.Module):
     def __init__(self):
         super().__init__()
+        
         self.conv1 = AlshConv2d(in_channels=1, out_channels=32, kernel_size=3, stride=1, padding=0, dilation=1, bias=True, is_first_layer=True, is_last_layer=True, num_hashes=5, num_tables=8, max_bits=16, hash_table=SRPTable)
-        # self.conv2 = None # dynamically set input channels for this
+        self.relu1 = nn.ReLU()
+        self.maxpool1 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        #self.conv2 = Conv.ALSHConv2d(in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=1)
         self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=1)
-        self.dropout1 = nn.Dropout(p=0.25)
-        self.dropout2 = nn.Dropout(p=0.5)
-        # self.fc1 = None # dynamically set input channels for this
-        self.fc1 = nn.Linear(in_features=9216, out_features=128)
-        self.fc2 = nn.Linear(in_features=128, out_features=10)
+        self.relu2 = nn.ReLU()
+        self.maxpool2 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        self.conv3 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1)
+        self.relu3 = nn.ReLU()
+
+        self.flatten = nn.Flatten()
+        self.fc1 = nn.Linear(64 * 7 * 7, 64)
+        self.relu4 = nn.ReLU()
+        self.fc2 = nn.Linear(64, 10)
 
     def forward(self, x):
         x = self.conv1(x)
-        x = F.relu(x)
-        # self.conv2 = AlshConv2d(in_channels=x.size(1), out_channels=64, kernel_size=3, stride=1, padding=0, dilation=1, bias=True, is_first_layer=False, is_last_layer=False, num_hashes=5, num_tables=8, max_bits=16, hash_table=SRPTable)
-        # self.conv2 = nn.Conv2d(in_channels=x.size(1), out_channels=64, kernel_size=3, stride=1)
+        x = self.relu1(x)
+        x = self.maxpool1(x)
         x = self.conv2(x)
-        x = F.relu(x)
-        x = F.max_pool2d(x, kernel_size=2)
-        x = self.dropout1(x)
-        x = torch.flatten(x, 1)
-        self.fc1 = nn.Linear(in_features=9216, out_features=128)
+        x = self.relu2(x)
+        x = self.maxpool2(x)
+        x = self.conv3(x)
+        x = self.relu3(x)
+        x = self.flatten(x)
         x = self.fc1(x)
-        x = F.relu(x)
-        x = self.dropout2(x)
+        x = self.relu4(x)
         x = self.fc2(x)
-        output = F.log_softmax(x, dim=1)
-        return output
+        return x
 
         
 
